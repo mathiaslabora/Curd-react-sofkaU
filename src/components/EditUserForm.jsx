549c2e88ba0311d6c774e466b7b4def3bfb94e37
Mathias Labora <mathiaslabora@gmail.com>
2022-02-23 01:05:36 -0300

    finalizando video

---
+++ src/components/EditUserForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const EditUserForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    defaultValues: props.currentUser,
+  });
+
+  setValue("name", props.currentUser.name);
+  setValue("userName", props.currentUser.userName);
+
+  const onSubmit = (data, e) => {
+      data.id = props.currentUser.id
+    props.updateUser(props.currentUser.id, data);
+
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Nombre</label>
+      <input
+        type="text"
+        {...register("name", {
+          required: true,
+        })}
+      />
+      {errors.name?.type === "required" && <div>campo requerido</div>}
+
+      <label>Nombre de usuario</label>
+      <input
+        type="text"
+        {...register("userName", {
+          required: true,
+          message: "campo requerido",
+        })}
+      />
+      {errors.userName?.type === "required" && <div>campo requerido</div>}
+      <button type="submit">Editar Usuario!</button>
+    </form>
+  );
+};
+
+export default EditUserForm;
--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const UserTable = (props) => (
     
+
     <table>
       <thead>
         <tr>
@@ -15,10 +16,11 @@ const UserTable = (props) => (
         props.users.map((user) => (
           <tr key={user.id}>
             <td>{user.name}</td>
-            <td>{user.username}</td>
+            <td>{user.userName}</td>
             <td>
               <button
                className="button muted-button"
+               onClick={()=>{props.editRow(user)}}
                >Edit</button>
               <button 
               className="button muted-button"
